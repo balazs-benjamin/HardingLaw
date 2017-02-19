84aa88cbe7fc4fb18a959a69c358f3006ae3fdae
balazs-benjamin <balazs_benjamin@outlook.com>
2017-02-19 15:51:38 +0100

    Initial Commit

---
+++ HardingLaw/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  HardingLaw
+//
+//  Created by mobile developer on 2017. 02. 19..
+//  Copyright © 2017. mobile developer. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ HardingLawTests/HardingLawTests.swift
@@ -0,0 +1,36 @@
+//
+//  HardingLawTests.swift
+//  HardingLawTests
+//
+//  Created by mobile developer on 2017. 02. 19..
+//  Copyright © 2017. mobile developer. All rights reserved.
+//
+
+import XCTest
+@testable import HardingLaw
+
+class HardingLawTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
